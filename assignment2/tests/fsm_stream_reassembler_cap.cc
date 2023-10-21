788c2f0be7144a1d2c377f5755277a2269b2b011
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2023-10-21 17:29:25 +0530

    Add reassembler tests

---
+++ assignment2/tests/fsm_stream_reassembler_cap.cc
@@ -0,0 +1,93 @@
+#include "byte_stream.hh"
+#include "fsm_stream_reassembler_harness.hh"
+#include "stream_reassembler.hh"
+#include "util.hh"
+
+#include <exception>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    try {
+        {
+            ReassemblerTestHarness test{2};
+
+            test.execute(SubmitSegment{"ab", 0});
+            test.execute(BytesAssembled(2));
+            test.execute(BytesAvailable("ab"));
+
+            test.execute(SubmitSegment{"cd", 2});
+            test.execute(BytesAssembled(4));
+            test.execute(BytesAvailable("cd"));
+
+            test.execute(SubmitSegment{"ef", 4});
+            test.execute(BytesAssembled(6));
+            test.execute(BytesAvailable("ef"));
+        }
+
+        {
+            ReassemblerTestHarness test{2};
+
+            test.execute(SubmitSegment{"ab", 0});
+            test.execute(BytesAssembled(2));
+
+            test.execute(SubmitSegment{"cd", 2});
+            test.execute(BytesAssembled(2));
+
+            test.execute(BytesAvailable("ab"));
+            test.execute(BytesAssembled(2));
+
+            test.execute(SubmitSegment{"cd", 2});
+            test.execute(BytesAssembled(4));
+
+            test.execute(BytesAvailable("cd"));
+        }
+
+        {
+            ReassemblerTestHarness test{1};
+
+            test.execute(SubmitSegment{"ab", 0});
+            test.execute(BytesAssembled(1));
+
+            test.execute(SubmitSegment{"ab", 0});
+            test.execute(BytesAssembled(1));
+
+            test.execute(BytesAvailable("a"));
+            test.execute(BytesAssembled(1));
+
+            test.execute(SubmitSegment{"abc", 0});
+            test.execute(BytesAssembled(2));
+
+            test.execute(BytesAvailable("b"));
+            test.execute(BytesAssembled(2));
+        }
+
+        {
+            ReassemblerTestHarness test{3};
+            for (unsigned int i = 0; i < 99997; i += 3) {
+                const string segment = {char(i), char(i + 1), char(i + 2), char(i + 13), char(i + 47), char(i + 9)};
+                test.execute(SubmitSegment{segment, i});
+                test.execute(BytesAssembled(i + 3));
+                test.execute(BytesAvailable(segment.substr(0, 3)));
+            }
+        }
+
+        {
+            StreamReassembler test5(4);
+            test5.push_substring("a", 0, false);
+            test5.push_substring("c", 2, false);
+            test5.push_substring("e", 4, false);
+            test5.push_substring("b", 1, false);
+            test5.push_substring("d", 3, false);
+
+
+        }
+
+    } catch (const exception &e) {
+        cerr << "Exception: " << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
